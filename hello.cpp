4732b6d7d26290f18d14a51ded7f44cfa5ac22cf
deji4things2000 <deji4things2000@gmail.com>
2025-09-16 23:28:58 -0400

    Problem Set 0- COSC 169 Parallel Optimization in Robotics

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+
+#include <cstdio>
+int main() {
+    // C and C++ programs start executing from the main function
+    // the main function returns an integer (it needs to be "typed")
+    printf("Hello, Dartmouth!\n");
+    return 0;
+}
