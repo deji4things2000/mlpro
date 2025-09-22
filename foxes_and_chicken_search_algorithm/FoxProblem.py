906efd97a811b4fb555dc8741a2df7ab95a49817
AADE <deji4things2000@gmail.com>
2025-09-22 18:32:30 -0400

    foxes_and_chicken algorithm for uninformed search

--- foxes_and_chicken_search_algorithm/FoxProblem.py
+++ foxes_and_chicken_search_algorithm/FoxProblem.py
@@ -1,18 +1,60 @@
 class FoxProblem:
     def __init__(self, start_state=(3, 3, 1)):
         self.start_state = start_state
-        self.goal_state = (0, 0, 0)
+        self.goal_state = (0, 0, 0) #the goal is to have zero chicken, foxes, and boats at the start state.
+        self.total_chickens = start_state[0]
+        self.total_foxes = start_state[1]
+        self.boat_capacity = 2
 
-        # you might want to add other things to the problem,
-        #  like the total number of chickens (which you can figure out
-        #  based on start_state
-
-    # get successor states for the given state
     def get_successors(self, state):
-        # you write this part. I also had a helper function
-        #  that tested if states were safe before adding to successor list
+        successors = []
+        c, f, b = state # where c represents chicken, f represents foxes, and b represents boat
+        if b == 1: # Boat is on starting side (i.e. b = 1)
+            for move_c in range(self.boat_capacity + 1):
+                for move_f in range(self.boat_capacity + 1):
+                    total_move = move_c + move_f
+                    if total_move > 0 and total_move <=self.boat_capacity:
+                        new_c = c - move_c
+                        new_f = f - move_f
+                        new_b = 0
 
-    # I also had a goal test method. You should write one.
+                        if self.is_safe((new_c, new_f, new_b)):
+                            successors.append((new_c, new_f, new_b))
+        else: # Boat is on starting side (i.e. b = 0)
+            for move_c in range(self.boat_capacity + 1):
+                for move_f in range(self.boat_capacity + 1):
+                    total_move = move_c + move_f
+                    if total_move > 0 and total_move <=self.boat_capacity:
+                        new_c = c + move_c
+                        new_f = f + move_f
+                        new_b = 1
+
+                        if self.is_safe((new_c, new_f, new_b)):
+                            successors.append((new_c, new_f, new_b))
+        return successors
+    def is_safe(self, state):
+        c, f, b = state #where c represents chicken, f represents foxes, and b represents boat
+        goal_c = self.total_chickens - c
+        goal_f = self.total_foxes - f
+
+        #Check bounds
+
+        if c < 0 or c > self.total_chickens or f < 0 or f > self.total_foxes:
+            return False
+        
+        # Check starting side safety
+        if c > 0 and f > c:
+            return False
+        
+        # Check goal side safety
+        if goal_c > 0 and goal_f > goal_c:
+            return False
+        
+        return True
+    
+    def goal_test(self, state):
+        return state == self.goal_state
+
 
     def __str__(self):
         string =  "Chickens and foxes problem: " + str(self.start_state)
--- foxes_and_chicken_search_algorithm/uninformed_search.py
+++ foxes_and_chicken_search_algorithm/uninformed_search.py
@@ -2,35 +2,100 @@
 from collections import deque
 from SearchSolution import SearchSolution
 
-# you might find a SearchNode class useful to wrap state objects,
-#  keep track of current depth for the dfs, and point to parent nodes
 class SearchNode:
-    # each search node except the root has a parent node
-    # and all search nodes wrap a state object
 
     def __init__(self, state, parent=None):
-        # you write this part
+        self.state = state
+        self.parent = parent
 
-# you might write other helper functions, too. For example,
-#  I like to separate out backchaining, and the dfs path checking functions
 
 def bfs_search(search_problem):
+    solution = SearchSolution(search_problem, "BFS")
 
-# Don't forget that your dfs function should be recursive and do path checking,
-#  rather than memoizing (no visited set!) to be memory efficient
+    start_node = SearchNode(search_problem.start_state)
+    frontier = deque([start_node])
+    explored = set()
 
-# We pass the solution along to each new recursive call to dfs_search
-#  so that statistics like number of nodes visited or recursion depth
-#  might be recorded
-def dfs_search(search_problem, depth_limit=100, node=None, solution=None):
+    while frontier:
+        node = frontier.popleft()
+        state = node.state
+
+        if state in explored:
+            continue
+
+        explored.add(state)
+        solution.nodes_visited += 1
+
+        if search_problem.goal_test(state):
+            # Backchan]in to get path
+            path = []
+            current = node
+            while current:
+                path.append(current.state)
+                current = current.parent
+            path.reverse()
+            solution.path = path
+            return solution
+        
+        for successor in search_problem.get_successors(state):
+            if successor not in explored:
+                child_node = SearchNode(successor, node)
+                frontier.append(child_node)
+    return solution
+
+def dfs_search(search_problem):
+    return dfs_search_recursive(search_problem)
+
+
+def dfs_search_recursive(search_problem, depth_limit=100, node=None, solution=None):
     # if no node object given, create a new search from starting state
     if node == None:
         node = SearchNode(search_problem.start_state)
         solution = SearchSolution(search_problem, "DFS")
 
-    # you write this part
+    state = node.state
+    solution.nodes_visited +=1
 
+    if search_problem.goal_test(state):
+        path = []
+        current = node
+        while current:
+            path.append(current.state)
+            current = current.parent
+        path.reverse()
+        solution.path = path
+        return solution
+    
+    if depth_limit <= 0:
+        return solution
+    
+    # Path Checking - only checking current path
+    current_path = set()
+    temp = node
+    while temp:
+        current_path.add(temp.state)
+        temp = temp.parent
 
+    for successor in search_problem.get_successors(state):
+        if successor not in current_path:
+            child_node = SearchNode(successor, node)
+            result = dfs_search_recursive(search_problem, depth_limit-1, child_node, solution)
+            if result.path:
+                return result
+            
+    return solution
 
-def ids_search(search_problem, depth_limit=100):
-    # you write this part
+def ids_search(search_problem):
+    solution = SearchSolution(search_problem, "IDS")
+    depth_limit = 100
+
+    for depth in range(depth_limit + 1):
+        temp_solution = SearchSolution(search_problem, "DFS")
+        result = dfs_search_recursive(search_problem, depth)
+        solution.nodes_visited += result.nodes_visited
+
+        if result.path:
+            solution.path = result.path
+            return solution
+    
+    return solution
